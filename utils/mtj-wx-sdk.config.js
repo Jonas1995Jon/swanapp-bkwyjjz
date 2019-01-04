c3aba406bd6c75bb8cc4639f3de0931f603486f5
Jonas <jiangjian@bangkaoedu.com>
2019-01-04 12:01:36 +0800

    first time add this project

---
+++ utils/mtj-wx-sdk.config.js
@@ -0,0 +1,17 @@
+/**
+ * @file 百度移动统计配置文件
+ */
+
+module.exports = {
+  /**
+   * 从百度移动统计获取的AppKey
+   * @type {string}
+   */
+  appKey: 'AsHybR4i7vHA4auvUAQ74FWd8gsTYuZs',
+
+  /**
+   * 是否获取当前的地理位置、速度
+   * @type {boolean}
+   */
+  getLocation: false
+};
